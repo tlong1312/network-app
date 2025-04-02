ef82ceb2a31eda83d2f5f1f21b9122816243dc35
PhanDuyNhan <phanduynhan1006@gmail.com>
2025-04-02 18:50:06 +0700

    them quan ly bai dang

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import './App.css';
 import Navbar from './components/HomePage/Navbar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchPage from "./components/SearchPage/searchPage";
-
+import RegisterPage from './components/RegisterPage/RegisterPage';
+import AdminPost from './components/AdminPost/AdminPost';
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/search" element={<SearchPage />} />  
+        <Route path="/adminPost" element={<AdminPost />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
   );
