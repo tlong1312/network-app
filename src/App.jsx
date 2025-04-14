6ce9a6012001117db2eded63346c93a48017b970
Dang Tieu Long <dangtieulong362@gmail.com>
2025-04-14 22:39:38 +0700

    fix merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-import SearchPage from "./components/SearchPage/searchPage";
-<<<<<<< HEAD
-import RegisterPage from './components/RegisterPage/RegisterPage';
-=======
-import HomePage from './components/HomePage/HomePage';
->>>>>>> tlong
 
 
-=======
 import MainLayout from "./components/Layouts/MainLayout";
 import AdminLayout from "./components/Layouts/AdminLayout";
 import SearchPage from "./components/HomePage/SearchPage/searchPage";
@@ -20,21 +12,11 @@ import HomePage from "./components/HomePage/HomePage";
 import StatisticalManagement from "./components/Admin/Content/StatisticalManagement";
 import UserManagement from "./components/Admin/Content/UserManagement";
 import PostManagement from "./components/Admin/Content/PostManagement";
->>>>>>> duphan
 function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-      <Routes>
->>>>>>> tlong
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-=======
+
         {/* Public Routes */}
         <Route element={<MainLayout />}>
           <Route index element={<HomePage />} />
@@ -53,8 +35,6 @@ function App() {
             <Route path="post-management" element={<PostManagement />} />
           </Route>
         </Route>
-
->>>>>>> duphan
       </Routes>
     </Router>
   );
